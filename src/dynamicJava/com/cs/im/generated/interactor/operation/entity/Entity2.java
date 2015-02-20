207b2142ccab9eb581d6bb5866c0cd0975e382ae
aayush <aayush@inbetween.co.in>
2015-02-20 17:08:51 +0530

    Added Entities

---
+++ src/dynamicJava/com/cs/im/generated/interactor/operation/entity/Entity2.java
@@ -0,0 +1,26 @@
+// template will be used to create both Item and Tag type entity-class
+
+package com.cs.im.generated.interactor.operation.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+
+
+import javax.persistence.CascadeType;
+import java.util.Set;
+
+import com.cs.im.core.interactor.operation.entity.base.Item;
+
+@Entity
+public class Entity2 extends Item implements Serializable{
+
+	
+
+  @Override
+  public String toString(){
+    String data = "Entity2 [ ";
+		data += "]";
+		return data;
+  }
+}
